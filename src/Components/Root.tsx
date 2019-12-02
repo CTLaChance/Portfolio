7ea3a679a0682d341e2fce56775eceeb24295533
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-02 00:31:39 -0600

    Portfolio init commit.

---
+++ src/Components/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Root.scss'
+
+const Root: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1 id="title">CHRISTOPHER LACHANCE</h1>
+    </React.Fragment>
+  );
+}
+
+export default Root;
