4f6b896d701dd64d6ecea306662a6858267f19a2
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-04 23:06:59 -0600

    Finally got BabylonJS working.

--- src/Components/Root.tsx
+++ src/Components/Root.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 import './Root.scss'
+import Visualizer from './Visualizer';
 
 const Root: React.FC = () => {
+  const calculateYearsOld = () => {
+    return moment().diff(moment("1994-12-06"), 'years');
+  };
+
   return (
     <React.Fragment>
+      <Visualizer />
       <h1 id="title">CHRISTOPHER LACHANCE</h1>
     </React.Fragment>
   );
