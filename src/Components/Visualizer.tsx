81f06c90ec37a7ffe90271f379556515336ff68a
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-09 22:00:30 -0600

    Swapped id tag on canvas elemnt to use a reference and classname.
    Thanks JoahG for the suggestion!

--- src/Components/Visualizer.tsx
+++ src/Components/Visualizer.tsx
@@ -3,8 +3,11 @@ import * as BABYLON from 'babylonjs';
 import './Visualizer.scss';
 
 class Visualizer extends React.Component {
+    private canvas : HTMLCanvasElement;
+
     componentDidMount() {
-        const canvas = document.getElementById("visualizer") as HTMLCanvasElement;
+        // const canvas = document.getElementById("visualizer") as HTMLCanvasElement;
+        const canvas = this.canvas;
         const engine = new BABYLON.Engine(canvas, true);
         
         // createScene function that creates and return the scene
@@ -17,15 +20,19 @@ class Visualizer extends React.Component {
             let camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, -10), scene);
             camera.setTarget(BABYLON.Vector3.Zero());
 
-            // Lighting Properties
-            let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 0, 1), scene);
-            light.diffuse = light.specular = light.groundColor = new BABYLON.Color3(0.929,0.122,0.012);
-
-            var sphere = BABYLON.Mesh.CreateSphere('sphere1', 1, 1, scene);
             // Particle System
             let particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
-            particleSystem.emitter = sphere;
+            // particleSystem.emitter = particleSystem.createPointEmitter(BABYLON.Vector3.Zero(), BABYLON.Vector3.Zero());
+            particleSystem.emitter = BABYLON.Vector3.Zero();
             particleSystem.particleTexture = new BABYLON.Texture('/assets/pixel.png', scene);
+            
+            particleSystem.minSize = 0.01;
+            particleSystem.maxSize = 0.02;
+            
+            particleSystem.minLifeTime = 3.5;
+            particleSystem.maxLifeTime = 5;
+
+            particleSystem.color1 = particleSystem.color2 = new BABYLON.Color4(0.929, 0.122, 0.012, 1);
             
             particleSystem.start();
             
@@ -48,7 +55,8 @@ class Visualizer extends React.Component {
     }
 
     render () {
-        return <canvas id="visualizer"/>;
+        return <canvas className="visualizer" ref={(element : HTMLCanvasElement) => this.canvas = element}/>;
+        // return <canvas id="visualizer"/>;
     }
 }
 
