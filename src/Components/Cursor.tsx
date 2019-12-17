99ac80c4cce2be691f329fd237990bc219eb25f6
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-16 18:50:15 -0600

    Fixed some bugs with the cursor.
    Hid scrollbars that appeared when moving mouse outside cursor.
    Tweaked cursor styling.
    Cursor now hides when leaving canvas.

--- src/Components/Cursor.tsx
+++ src/Components/Cursor.tsx
@@ -7,9 +7,20 @@ class Cursor extends React.Component {
     componentDidMount() {
         const cursor = this.cursor;
 
+
+        // Hide the cursor when leaving.
+        document.addEventListener('mouseleave', event => {
+            cursor.hidden = true;
+        });
+
+        document.addEventListener('mouseenter', event => {
+            cursor.hidden = false;
+        });
+
+        // Update the cursor div position on mouse move.
         document.addEventListener('mousemove', event => {
             cursor?.setAttribute("style", `top: ${event.pageY - (cursor.offsetHeight/2)}px; left: ${event.pageX - (cursor.offsetWidth/2)}px;`);
-        })
+        });
     }
 
     render() {
