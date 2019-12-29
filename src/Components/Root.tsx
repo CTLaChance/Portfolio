6772fae298d7552f5946049f087c07dc9636f616
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-28 20:12:59 -0600

    Created a portfolio component.

--- src/Components/Root.tsx
+++ src/Components/Root.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './Root.scss';
 import Visualizer from './Visualizer';
 import Cursor from './Cursor';
+import Portfolio from './Portfolio';
 
 const Root: React.FC = () => {
   return (
     <React.Fragment>
-      <h1 id="title">CHRISTOPHER LACHANCE</h1>
+      <Portfolio />
       <Visualizer />
       <Cursor />
     </React.Fragment>
