6b7ed711e3d58bf9241a4cbed04281757d1fb7ba
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-28 20:13:11 -0600

    Create Portfolio.tsx

---
+++ src/Components/Portfolio.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Portfolio.scss';
+
+class Portfolio extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div id="portfolio">
+                    <h1 id="name">CHRISTOPHER LACHANCE</h1>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Portfolio;
