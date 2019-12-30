62db051255d962587db446aa588822c8ca222a61
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-29 18:41:20 -0600

    Fleshing out bio.
    Cursor is now hidden by default. Will appear when you move your mouse.

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -7,6 +7,28 @@ class Portfolio extends React.Component {
             <React.Fragment>
                 <div id="portfolio">
                     <h1 id="name">CHRISTOPHER LACHANCE</h1>
+                    <div id="about">
+                        <h2>About</h2>
+                        <p>
+                            I am an artist and engineer with a passion for music, aesthetic design, and minimalism.
+                        </p>
+                        <p>
+                            This site was created as an exercise in learning React, Typescript, and BabylonJS. The source code is available on <a href="https://github.com/CTLaChance/Portfolio">GitHub.</a>
+                        </p>
+                    </div>
+                    <div id="projects">
+                        <h2>Projects</h2>
+                        <div className="portfolio-card">Card 1</div>
+                        <div className="portfolio-card">Card 2</div>
+                        <div className="portfolio-card">Card 3</div>
+                        <div className="portfolio-card">Card 4</div>
+                        <div className="portfolio-card">Card 5</div>
+                        <div className="portfolio-card">Card 6</div>
+                        <div className="portfolio-card">Card 7</div>
+                        <div className="portfolio-card">Card 8</div>
+                        <div className="portfolio-card">Card 9</div>
+                        <div className="portfolio-card">Card 10</div>
+                    </div>
                 </div>
             </React.Fragment>
         )
