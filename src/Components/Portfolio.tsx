494f5c92d5b39ec22e30a46a6f6080204ff49756
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-30 21:34:07 -0600

    Playing around with SwiperJS.

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import Swiper from 'swiper';
 import './Portfolio.scss';
 
 class Portfolio extends React.Component {
+    componentDidMount() {
+        let mySwiper = new Swiper('#carousel', {
+            init: true,
+
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
-                <div id="portfolio">
-                    <h1 id="name">CHRISTOPHER LACHANCE</h1>
-                    <h2 className="augmented-header" augmented-ui="bl-clip tr-clip exe">PROJECTS</h2>
+                <div id="name">CHRISTOPHER LACHANCE</div>
+                <div id="carousel">
+                    <div className="swiper-wrapper">
+                        <div className="swiper-slide portfolio-card">Card 1</div>
+                        <div className="swiper-slide portfolio-card">Card 2</div>
+                        <div className="swiper-slide portfolio-card">Card 3</div>
+                        <div className="swiper-slide portfolio-card">Card 4</div>
+                        <div className="swiper-slide portfolio-card">Card 5</div>
+                        <div className="swiper-slide portfolio-card">Card 6</div>
+                        <div className="swiper-slide portfolio-card">Card 7</div>
+                    </div>
                 </div>
             </React.Fragment>
         )
