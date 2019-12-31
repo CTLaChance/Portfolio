980a5f876a9ceba142c0e2faeb0463b22d80fd77
Christopher LaChance <CTLaChance@users.noreply.github.com>
2019-12-30 22:41:12 -0600

    Formated carousel cards.

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Swiper from 'swiper';
 import './Portfolio.scss';
+import 'swiper/swiper.scss'
 
 class Portfolio extends React.Component {
     componentDidMount() {
-        let mySwiper = new Swiper('#carousel', {
+        let mySwiper = new Swiper('.swiper-container', {
             init: true,
-
+            freeMode: true,
+            slidesPerView: 3,
+            spaceBetween: 50,
+            centeredSlides: true
         });
     }
 
@@ -14,15 +18,15 @@ class Portfolio extends React.Component {
         return (
             <React.Fragment>
                 <div id="name">CHRISTOPHER LACHANCE</div>
-                <div id="carousel">
+                <div className="swiper-container">
                     <div className="swiper-wrapper">
-                        <div className="swiper-slide portfolio-card">Card 1</div>
-                        <div className="swiper-slide portfolio-card">Card 2</div>
-                        <div className="swiper-slide portfolio-card">Card 3</div>
-                        <div className="swiper-slide portfolio-card">Card 4</div>
-                        <div className="swiper-slide portfolio-card">Card 5</div>
-                        <div className="swiper-slide portfolio-card">Card 6</div>
-                        <div className="swiper-slide portfolio-card">Card 7</div>
+                        <div className="swiper-slide">Card 1</div>
+                        <div className="swiper-slide">Card 2</div>
+                        <div className="swiper-slide">Card 3</div>
+                        <div className="swiper-slide">Card 4</div>
+                        <div className="swiper-slide">Card 5</div>
+                        <div className="swiper-slide">Card 6</div>
+                        <div className="swiper-slide">Card 7</div>
                     </div>
                 </div>
             </React.Fragment>
