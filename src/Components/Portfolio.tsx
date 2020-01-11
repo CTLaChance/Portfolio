e57fce842855bd2a7336652b51b23b82c2d8e50c
Christopher LaChance <CTLaChance@users.noreply.github.com>
2020-01-11 12:53:35 -0600

    Woo! Got smooth drag scrolling working.

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Swiper from 'swiper';
+// @ts-ignore
+import ScrollBooster from 'scrollbooster';
 import './Portfolio.scss';
 import 'swiper/swiper.scss'
 
@@ -71,10 +73,27 @@ class Portfolio extends React.Component {
 
         if(this.state.detailsOpened)
         {
+            setTimeout(function () {
+                const viewport = document.getElementById('details-page');
+                const content = document.getElementById('scroll-wrapper');
+
+                new ScrollBooster({
+                    viewport,
+                    content,
+                    direction: "vertical",
+                    onUpdate: (state: { position: { x: number; y: number; }; }) => {
+                        content.style.transform = `translate(
+                            ${-state.position.x}px,
+                            ${-state.position.y}px
+                        )`;
+                    }
+                });
+            }, 1);
+
             details =
             <div id="details-page">
-                <div id="details-info">
-                    <dl>
+                <div id="scroll-wrapper">
+                    <dl id="details-info">
                         <dt>PROJECT NAME:</dt>
                         <dd>Lorem Ipsum</dd>
                         
@@ -85,48 +104,21 @@ class Portfolio extends React.Component {
                         <dd><a href="https://ctlachance.com">https://ctlachance.com</a></dd>
 
                         <dt>COLLABORATORS:</dt>
-                        <dd>Null</dd>
+                        <dd>NULL</dd>
 
                         <dt>SUMMARY:</dt>
-                        <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam minima at nesciunt omnis a itaque dolores unde ut officia facere soluta aliquid provident tempora, illo assumenda quas ducimus sapiente non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo provident qui magni accusamus aperiam aut, beatae iusto modi repellendus quisquam! Voluptatem, sequi libero quaerat dolores labore unde non consequuntur ex!</dd>
+                        <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam minima at nesciunt omnis a itaque dolores unde ut officia facere soluta aliquid provident tempora, illo assumenda quas ducimus sapiente non.</dd>
                     </dl>
-                </div>
-
-                <div id="details-pictures" className="swiper-container">
-                    <div className="swiper-wrapper">
-                        <div className="swiper-slide">
-                            <img src={this.projects[0].card} alt="Project Media"></img>
-                        </div>
-                        <div className="swiper-slide">
-                            <img src={this.projects[0].card} alt="Project Media"></img>
-                        </div>
-                        <div className="swiper-slide">
-                            <img src={this.projects[0].card} alt="Project Media"></img>
-                        </div>
-                        <div className="swiper-slide">
-                            <img src={this.projects[0].card} alt="Project Media"></img>
-                        </div>
-                        <div className="swiper-slide">
-                            <img src={this.projects[0].card} alt="Project Media"></img>
-                        </div>
-                        <div className="swiper-slide">
-                            <img src={this.projects[0].card} alt="Project Media"></img>
-                        </div>
+                    <div id="details-pictures">
+                        <img src={this.projects[0].card} alt="Project Media"></img>
+                        <img src={this.projects[0].card} alt="Project Media"></img>
+                        <img src={this.projects[0].card} alt="Project Media"></img>
+                        <img src={this.projects[0].card} alt="Project Media"></img>
+                        <img src={this.projects[0].card} alt="Project Media"></img>
+                        <img src={this.projects[0].card} alt="Project Media"></img>
                     </div>
                 </div>
             </div>;
-
-            setTimeout(function () {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                let detailsPicturesSwiper = new Swiper('#details-pictures', {
-                    init: true,
-                    freeMode: true,
-                    initialSlide: 1,
-                    slidesPerView: "auto",
-                    spaceBetween: 50,
-                    centeredSlides: true,
-                });
-            }, 1);
         }
 
         return (
