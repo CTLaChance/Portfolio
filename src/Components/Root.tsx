e7dd938d31116cf0f2aa905f812e5fda23ee5032
Christopher LaChance <CTLaChance@users.noreply.github.com>
2020-03-04 21:09:44 -0600

    Refactoring + Name Animation
    Created a simple animation for each character in the name.
    Refactored some code.

--- src/Components/Root.tsx
+++ src/Components/Root.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './Root.scss';
 import Visualizer from './Visualizer';
-// import Cursor from './Cursor';
-// import Portfolio from './Portfolio';
+import Content from './Content';
 
 const Root: React.FC = () => {
   return (
     // Stack items from furthest to closest.
     <React.Fragment>
       <Visualizer />
-      <div id="content">
-        <h1 id="name">CHRISTOPHER LACHANCE</h1>
-        <svg id="chevron-down" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
-          <polyline points="6 9 12 15 18 9"></polyline>
-        </svg>
-      </div>
+      <Content />
     </React.Fragment>
   );
 }
