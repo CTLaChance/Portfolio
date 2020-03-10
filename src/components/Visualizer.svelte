cdc5bae968bb9d76621f38062acd70b063759b55
Christopher LaChance <CTLaChance@users.noreply.github.com>
2020-03-09 23:29:32 -0500

    Basic Svelte project up and running.

---
+++ src/components/Visualizer.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<style lang="scss">
+    $text-color: red;
+
+    #id1 {
+        color: $text-color;
+        border: 1px solid black;
+
+        #id2 {
+            color: #1c1c1c;
+        }
+    }
+</style>
+
+<div id="id1">
+    TEXT 1
+    <div id="id2">
+        TEXT 2
+    </div>
+</div>
