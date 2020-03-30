2c3f43f8c28c62b789ca8c99839208e5978da5fd
Christopher LaChance <8072374+CTLaChance@users.noreply.github.com>
2020-03-30 17:13:23 -0500

    Project media now shows.

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -41,17 +41,37 @@
         margin: 0 auto;
         padding: 16px;
 
-        .project-card {
+        .grid-card {
             background-size: cover;
             background-position: center;
             transition: transform .2s ease-in-out;
-
+            
             &:before {
                 content: "";
                 display: block;
                 height: 0;
                 width: 0;
                 padding-bottom: calc(9/16 * 100%);
+            }
+
+            &:hover {
+                cursor: pointer;
+                transform: scale(1.05, 1.05);
+            }
+        }
+
+        .media-card {
+            background-size: contain;
+            background-position: center;
+            background-repeat: no-repeat;
+            transition: transform .2s ease-in-out;
+            
+            &:before {
+                content: "";
+                display: block;
+                height: 0;
+                width: 0;
+                padding-bottom: 100%;
             }
 
             &:hover {
@@ -70,11 +90,15 @@
     <div id="project-array">
         {#if folderIndex == null}
             {#each data as element, i}
-                <div id={element.name} class="project-card" style="background-image: url({element.card})" on:click={() => openFolder(i)}></div>
+                <div id={element.name} class="grid-card" style="background-image: url({element.card})" on:click={() => openFolder(i)}></div>
+            {/each}
+        {:else if projectIndex == null}
+            {#each data[folderIndex].projects as element, i}
+                <div id={element.name} class="grid-card" style="background-image: url({element.card})" on:click={() => openProject(i)}></div>
             {/each}
         {:else}
-            {#each data[folderIndex].projects as element, i}
-                <div id={element.name} class="project-card" style="background-image: url({element.card})" on:click={() => openProject(i)}></div>
+            {#each data[folderIndex].projects[projectIndex].media as element}
+                <div id={element.name} class="media-card" style="background-image: url({element})"></div>
             {/each}
         {/if}
     </div>
