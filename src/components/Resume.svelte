cf0ac7b77d39e0cbaf5f2df996c5ad625e206046
Christopher LaChance <8072374+CTLaChance@users.noreply.github.com>
2020-10-18 13:58:39 -0500

    Update Resume.svelte

--- src/components/Resume.svelte
+++ src/components/Resume.svelte
@@ -1,9 +1,13 @@
 <style lang="scss">
     #resume {
         position: absolute;
-        top: 10%;
+        top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
+
+        width: 8.5in;
+        height: 11in;
+        padding: 16px;
 
         font-family: 'Roboto';
         box-shadow: 0px 4px 8px rgba($color: black, $alpha: 0.25);
@@ -11,8 +15,60 @@
     }
 </style>
 
-
 <div id="resume">
-    <h1>CHRISTOPHER LACHANCE</h1>
-    <h2>Software Engineer</h2>
+    <div id="ctl">
+        <div>CHRISTOPHER LACHANCE</div>
+        <div>Software Engineer</div>
+        <ul>
+            <li>https://ctlachance.com</li>
+            <li>christopher.t.lachance@gmail.com</li>
+            <li>https://www.linkedin.com/in/ctlachance/</li>
+        </ul>
+    </div>
+
+    <div id="experience">
+        <h1>Experience</h1>
+        <div id="ags">
+            <h2>AGS, LLC</h2>
+            <h3>Software Engineer</h3>
+            <ul>
+                <li>Designed and implemented critical game logic for complex math models</li>
+                <li>Worked collaboratively with contributors of multiple disciplines artists, musicians, and game designers</li>
+                <li>Handled various DevOps challenges that arose from working in a small team environment</li>
+                <li>Lead Engineer for the following 9 games:</li>
+                <ul>
+                    <li>Pharaoh Sun</li>
+                    <li>Wild Tiki</li>
+                    <li>Dragon Blast</li>
+                    <li>Freedom 7</li>
+                    <li>8 Riches</li>
+                    <li>Shining Dragon</li>
+                    <li>Burning Tiger</li>
+                    <li>Bao Bei Cash</li>
+                    <li>Temple Cash</li>
+                </ul>
+            </ul>
+        </div>
+        <div id="miscellaneous">
+            <h2>Freelance Software Engineering</h2>
+            <h3>Front End Developer / Software Engineer / Graphic Designer</h3>
+            <ul>
+                <li>Designed and developed the website, 12+ book covers, and various other branded materials for Algebra Readiness Educators, LLC</li>
+                <li>Contributed features and bugfixes to the open source NASA Trick Simulation Environment</li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="skills">
+        <h1>Skills</h1>
+        <p>JavaScript ES6+, C#, Lua, Java, TypeScript, C++, HTML5, CSS3, SCSS, XML, Markdown, JSON, NodeJS, Svelte, React, Git, GitHub, SVN, Linux, Bash, Unity 3D, Photoshop, Virtual Reality</p>
+    </div>
+
+    <div id="education">
+        <h1>Education</h1>
+        <div>B.S. Computer Science</div>
+        <div>Lamar University</div>
+        <div>Beaumont, TX</div>
+        <div>December 2016</div>
+    </div>
 </div>
